71e294320d5725e8cab76768ae17041dc9c7610b
Francisco Beltrán <currob1999@gmail.com>
2021-03-23 19:46:14 +0100

    Añadiendo interfaces de la aplicación

--- App/app/src/main/java/com/example/prueba/PantallaLigas.kt
+++ App/app/src/main/java/com/example/prueba/PantallaLigas.kt
@@ -1,19 +1,16 @@
 package com.example.prueba
 
 import android.os.Bundle
-import android.view.View
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 
-class Registro : AppCompatActivity() {
+class PantallaLigas : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro)
+        setContentView(R.layout.activity_pantalla_ligas)
     }
 
-    fun registrarUsuario(view: View){
 
-    }
 }
