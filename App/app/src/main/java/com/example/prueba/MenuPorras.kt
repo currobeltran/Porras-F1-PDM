f1f4ffe73bca739d5dc9c048d037007c94c45c4e
currobeltran <currob1999@gmail.com>
2021-03-25 19:28:20 +0100

    Modificando elementos interfaz

--- App/app/src/main/java/com/example/prueba/MenuPorras.kt
+++ App/app/src/main/java/com/example/prueba/MenuPorras.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class MenuPorras : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +14,26 @@ class MenuPorras : AppCompatActivity() {
 
     fun realizaPorra(view: View){
         val intentrealizaporra = Intent(this, RealizarPorra::class.java)
+        val idboton = view.id
+
+        if(idboton == findViewById<Button>(R.id.top3quali).id){
+            intentrealizaporra.putExtra("Titulo","Elija su top 3 para la clasificación")
+        }
+        else if(idboton == findViewById<Button>(R.id.top3carrera).id){
+            intentrealizaporra.putExtra("Titulo","Elija su top 3 para la carrera")
+        }
+        else if(idboton == findViewById<Button>(R.id.pitstop).id){
+            intentrealizaporra.putExtra("Titulo","Elija quien hará la parada más rápida")
+        }
+        else if(idboton == findViewById<Button>(R.id.vueltarapida).id){
+            intentrealizaporra.putExtra("Titulo","Elija quien hará la vuelta rápida")
+        }
+        else if(idboton == findViewById<Button>(R.id.escuderiaquali).id){
+            intentrealizaporra.putExtra("Titulo","Elija la escudería que conseguirá la pole")
+        }
+        else if(idboton == findViewById<Button>(R.id.escuderiacarrera).id){
+            intentrealizaporra.putExtra("Titulo","Elija la escudería que conseguirá la victoria")
+        }
 
         startActivity(intentrealizaporra)
     }
--- App/app/src/main/java/com/example/prueba/RealizarPorra.kt
+++ App/app/src/main/java/com/example/prueba/RealizarPorra.kt
@@ -4,10 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class RealizarPorra : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_realizar_porra)
+
+        val titulo = findViewById<TextView>(R.id.textView14)
+        titulo.text = intent.getStringExtra("Titulo")
     }
 }
